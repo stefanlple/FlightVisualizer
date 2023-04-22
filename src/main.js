4f33d69d6b33d89fa0d243f4270152b948c4071f
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-04-22 23:51:11 +0200

    basic setup

--- src/main.js
+++ src/main.js
@@ -13,36 +13,29 @@ import {
 } from "./eventfunctions/executeKeyAction.js";
 
 function main() {
-  console.log("fds");
   window.scene = new THREE.Scene();
-  window.scene.add(new THREE.AxesHelper(200));
+  window.scene.add(new THREE.AxesHelper(20));
+
+  window.camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  window.camera.position.set(30, 40, 50);
+  window.camera.lookAt(0, 0, 0);
+
   window.renderer = new THREE.WebGLRenderer({ antialias: true });
   window.renderer.setSize(window.innerWidth, window.innerHeight);
-  window.renderer.setClearColor();
-  window.renderer.shadowMap.enabled = true;
+  window.renderer.setClearColor(0xffffff);
 
   document.getElementById("3d_content").appendChild(window.renderer.domElement);
 
-  window.physics = new Physics(false);
-  window.physics.setup(0, -200, 0, 1 / 240, true);
-
-  const room = new Enviroment();
-  window.scene.add(room);
-
-  const camera = new Camera();
-  room.add(camera);
-  camera.instanciate(window);
-
   function mainLoop() {
-    const delta = clock.getDelta();
-
-    TWEEN.update();
-
     window.renderer.render(window.scene, window.camera);
 
     requestAnimationFrame(mainLoop);
   }
-
   mainLoop();
 }
 
