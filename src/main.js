da4d0a00781a7127475cdcc5acfcca3cf1b4cc45
Stefan Le <stefanle01@yahoo.de>
2023-04-23 23:58:13 +0200

    basic geometry

--- src/main.js
+++ src/main.js
@@ -30,9 +30,36 @@ function main() {
   window.renderer.setClearColor(0xffffff);
 
   const backgroundImage = new THREE.TextureLoader().load(
-    "./images/Water_SpecularMap_Earth.png"
+    "./images/Stars_Env.png"
   );
   window.scene.background = backgroundImage;
+
+  let cubeGeometry = new THREE.BoxGeometry(5, 5, 5);
+  let cubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true,
+  });
+  let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.position.set(-5, 3, 5);
+  window.scene.add(cube);
+
+  let sphereGeometry = new THREE.SphereGeometry(5, 10, 10);
+  let sphereMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0000ff,
+    wireframe: true,
+  });
+  let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.position.set(10, 5, -5);
+  window.scene.add(sphere);
+
+  let planeGeometry = new THREE.PlaneGeometry(40, 40);
+  let planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0xaaaaaa,
+    wireframe: true,
+  });
+  let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotation.set(THREE.MathUtils.degToRad(-90), 0, 0);
+  window.scene.add(plane);
 
   document.getElementById("3d_content").appendChild(window.renderer.domElement);
 
