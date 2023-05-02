7320242aad2b078cbc3c797706c0b851439407b6
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-05-02 17:52:38 +0200

    added latitude and longitude placements

--- src/objects/Globe.js
+++ src/objects/Globe.js
@@ -18,7 +18,11 @@ export default class Globe extends THREE.Group {
     const globeMaterial = new THREE.MeshLambertMaterial({
       color: 0xffffff,
       map: new THREE.TextureLoader().load(
-        "./src/images/No_Cloud_Earth_Map.jpg"
+        "./src/images/No_Cloud_Earth_Map.jpg",
+        (texture) => {
+          texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+          texture.offset.x = 1.5708 / (2 * Math.PI);
+        }
       ),
       bumpMap: new THREE.TextureLoader().load(
         "./src/images/Elevation_BumpMap_Earth.jpeg"
@@ -32,6 +36,25 @@ export default class Globe extends THREE.Group {
     const globe = new THREE.Mesh(globeGeometry, globeMaterial);
     this.add(globe);
 
+    function latLonToCart(inputLat, inputLng) {
+      const lat = inputLat * (Math.PI / 180);
+      const lng = inputLng * (Math.PI / 180);
+
+      const z = globeRadius * Math.cos(lat) * Math.cos(lng);
+      const x = globeRadius * Math.cos(lat) * Math.sin(lng);
+      const y = globeRadius * Math.sin(lat);
+      return [x, y, z];
+    }
+
+    const geometry = new THREE.SphereGeometry(2);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const ball = new THREE.Mesh(geometry, material);
+    ball.name = "ball";
+    ball.translateX(latLonToCart(34.052235, -118.243683)[0]);
+    ball.translateY(latLonToCart(34.052235, -118.243683)[1]);
+    ball.translateZ(latLonToCart(34.052235, -118.243683)[2]);
+    this.add(ball);
+
     const clouds = new THREE.Mesh(
       new THREE.SphereGeometry(globeRadius + 0.01),
       new THREE.MeshLambertMaterial({
@@ -41,7 +64,7 @@ export default class Globe extends THREE.Group {
         transparent: true,
       })
     );
-    //this.add(clouds);
+    this.add(clouds);
 
     const atmosphere = new THREE.Mesh(
       new THREE.SphereGeometry(globeRadius, 50, 50),
@@ -54,7 +77,5 @@ export default class Globe extends THREE.Group {
     );
 
     atmosphere.scale.set(1.05, 1.05, 1.05);
-
-    scene.add(atmosphere);
   }
 }
